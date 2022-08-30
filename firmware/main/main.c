33a580a7db0bf042f06b224edeeee90e957a244f
Marton Cserei <marton.cserei@nanolabs.ro>
2022-08-30 18:35:03 +0300

    strip down firmware

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,17 +1,7 @@
-#include "badge.h"
-#include "badge_test.h"
-#include "log.h"
-#include "spi_driver.h"
-#include "stress_testing.h"
-#include "utilities.h"
 
 #define MODULE_TAG "MAIN"
 
 void app_main(void) {
 
-    if (badge_init() == STATUS_OK) {
-        badge_loop();
-    }
-
-    badge_hibernate();
+    
 }
